9f25d2a889efc745226d2cd349477900839a727f
Jake Herman <jherman@forwardslope.com>
2023-08-09 16:57:23 -0700

    Add ultrasonic, camera; Increase robot speed

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,13 +4,20 @@
 
 package frc.robot;
 
+import java.util.ArrayList;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -23,16 +30,20 @@ public class Robot extends TimedRobot {
   private final WPI_TalonSRX m_rightDrive = new WPI_TalonSRX(2);
   private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftDrive, m_rightDrive);
   private final XboxController m_controller = new XboxController(0);
-  private final Timer m_timer = new Timer();
   private final Encoder m_leftEncoder = new Encoder(0, 1);
   private final Encoder m_rightEncoder = new Encoder(2, 3);
+  private final DigitalOutput m_ultrasonicEnable = new DigitalOutput(9);
+  private final AnalogPotentiometer m_ultrasonic1 = new AnalogPotentiometer(0);
 
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
-  public void robotInit() {}
+  public void robotInit() {
+    m_ultrasonicEnable.set(false);
+    CameraServer.startAutomaticCapture();
+  }
 
   /** This function is run once each time the robot enters autonomous mode. */
   @Override
@@ -49,7 +60,8 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during teleoperated mode. */
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.tankDrive(-0.5 * m_controller.getLeftY(), -0.5 * m_controller.getRightY());
+    m_robotDrive.tankDrive(-0.6 * m_controller.getLeftY(), -0.6 * m_controller.getRightY());
+    SmartDashboard.putNumber("Ultrasonic", m_ultrasonic1.get());
   }
 
   /** This function is called once each time the robot enters test mode. */
